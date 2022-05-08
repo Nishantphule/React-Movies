227b210d40daee63b9279d82623450c080c1ee70
Nishantphule <nishantphule12@gmail.com>
2022-05-08 14:48:59 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -70,6 +70,7 @@ function App() {
     <div className="App">
       <div className='main-container'>
       {movies.map(data => <Movies movie={data}/> )}</div> 
+      <div><AddMovie/></div>
     </div>
   );
 }
@@ -84,9 +85,9 @@ function Movies({movie}){
 
 const [show , setShow] = useState(true);
 
-    const paraStyles = {
-      display: show? "block" : "none"
-    }
+    // const paraStyles = {
+    //   display: show? "block" : "none"
+    // }
 
   return  (
      <div className='movie'>
@@ -95,13 +96,57 @@ const [show , setShow] = useState(true);
         <h1 className='title'>{movie.title}</h1>
         <p style={styles} className='rating'>⭐{movie.rating}</p>
       </div>
-      <button onClick={() => setShow(!show)} className="btn"></button>
-      <p className='summary' style={paraStyles} >{movie.description}</p>
+      <button onClick={() => setShow(!show)} className="btn">Toggle Summary</button>
+      {/* <p className='summary' style={paraStyles} >{movie.description}</p> */}
+
+
+      {show ? (<p className='summary' >{movie.description}</p>): "" }
+
       <Counter/>
      </div>
   );
  }
 
- 
+function AddMovie(){
+  const [Add, setMovie] = useState({
+    pic:"",
+    title:"",
+    rating:"",
+    description:""
+  })
+  const styles = {
+    color:"green"
+  }
+  Add.rating>7 ? 
+    styles.color = "green" :
+    styles.color = "red"
+
+const [show , setShow] = useState(true);
+
+    const paraStyles = {
+      display: show? "block" : "none"
+    }
+
+    
+  return(
+    <div className='add-movie'>
+    <input type="text" placeholder='Enter poster url' value={Add.pic} onChange={(e => setMovie({...Add,pic: e.target.value}))} ></input>
+    <input type="text" placeholder='Enter movie name' value={Add.title} onChange={(e => setMovie({...Add,title: e.target.value}))}></input>
+    <input type="text" placeholder='Enter movie rating' value={Add.rating} onChange={(e => setMovie({...Add,rating: e.target.value}))}></input>
+    <input type="text" placeholder='Enter poster summary' value={Add.description} onChange={(e => setMovie({...Add,description: e.target.value}))}></input>
+    <button>Add Movie</button>
+    <div className='movie'>
+      <img className="profilepic" src ={Add.pic} alt={Add.title} /> <br></br>
+      <div className='head'>
+        <h1 className='title'>{Add.title}</h1>
+        <p style={styles} className='rating'>⭐{Add.rating}</p>
+      </div>
+      <button onClick={() => setShow(!show)} className="btn">Toggle Summary</button>
+      <p className='summary' style={paraStyles} >{Add.description}</p>
+     </div>
+  </div>
+  )
+}
+
 
 export default App;
