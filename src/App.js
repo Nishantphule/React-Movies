3601ada1c9e2f4364ab1d135b3732666ddaa9cd3
Nishantphule <nishantphule12@gmail.com>
2022-05-13 22:01:47 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Home } from "./Home";
 import { Movies } from './Movies.js';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import ReactPlayer from "react-player";
 
 
 const INITIAL_MOVIE_buttonST = [
@@ -14,60 +15,70 @@ const INITIAL_MOVIE_buttonST = [
     pic: "https://gospeljingle.com/wp-content/uploads/2022/01/Pushpa_-The-Rise-2021.jpg",
     title: "Pushpa:The Rise - Part 1",
     rating: "7.6",
+    url:"https://youtu.be/pKctjlxbFDQ",
     description: "Story of Pushpa Raj, a lorry driver in Seshachalam forests of South India, set in the backdrop of red sandalwood smuggbuttonng. Red Sandalwood is endemic to South-Eastern Ghats (mountain range) of India."
   },
   {
     pic: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/348fa1129695937.61705209953c0.jpg",
     title: "The Batman",
     rating: "8.0",
+    url:"https://youtu.be/mqqft2x_Aa4",
     description: "When the Riddler, a sadistic serial killer, begins murdering key pobuttontical figures in Gotham, Batman is forced to investigate the city's hidden corruption and question his family's involvement."
   },
   {
     pic: "https://m.media-amazon.com/images/M/MV5BYzJmYzExZGEtMTUwYy00YzIyLWJmOTEtZWFkNTU0YThlYzdmXkEyXkFqcGdeQXVyODIwMDI1NjM@._V1_.jpg",
     title: "K.G.F: Chapter 1",
     rating: "8.2",
+    url:"https://youtu.be/-KfsY-qwBS0",
     description: "In the 1970s, a gangster goes undercover as a slave to assassinate the owner of a notorious gold mine."
   },
   {
     pic: "https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg",
     title: "Spider-Man: No Way Home",
     rating: "8.4",
+    url:"https://youtu.be/JfVOs4VSpmA",
     description: "With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man."
   },
   {
     pic: "https://m.media-amazon.com/images/M/MV5BOWE5ZjljZDEtYTZmNy00MGVlLWJjNjEtMWUwMzUyMDc5NTA3XkEyXkFqcGdeQXVyODgzMDMwODI@._V1_.jpg",
     title: "Bachchhan Paandey",
     rating: "6.8",
+    url:"https://youtu.be/cpNaGiBhXiM",
     description: "A budding director tries to research a merciless gangster for making a film on gangster-ism. But her secret attempts to conduct the research fail when she gets caught for snooping."
   },
   {
     pic: "https://m.media-amazon.com/images/I/61zgu8mImuL._AC_SY606_.jpg",
     title: "Joker",
     rating: "8.4",
+    url:"https://youtu.be/zAGVQLHvwOY",
     description: "A mentally troubled stand-up comedian embarks on a downward spiral that leads to the creation of an iconic villain."
   },
   {
     pic: "https://m.media-amazon.com/images/M/MV5BYmQxNmU4ZjgtYzE5Mi00ZDlhLTlhOTctMzJkNjk2ZGUyZGEwXkEyXkFqcGdeQXVyMzgxMDA0Nzk@._V1_.jpg",
     title: "Tumbbad",
     rating: "8.2",
+    url:"https://youtu.be/sN75MPxgvX8",
     description: "A mythological story about a goddess who created the entire universe. The plot revolves around the consequences when humans build a temple for her first-born."
   },
   {
     pic: "https://m.media-amazon.com/images/M/MV5BOWE1ZTMyM2QtMTNhNC00M2ZhLTg5ZTctNGZmZDM4YWQ5N2YwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
     title: "Godzilla vs. Kong",
     rating: "6.3",
+    url:"https://youtu.be/odM92ap8_c0",
     description: "The epic next chapter in the cinematic Monsterverse pits two of the greatest icons in motion picture history against each other--the fearsome Godzilla and the mighty Kong--with humanity caught in the balance."
   },
   {
     pic: "https://m.media-amazon.com/images/M/MV5BYzE3ODhiNzAtOWY4MS00NTdiLThmNDctNDM4NjRiNGFmYjI1XkEyXkFqcGdeQXVyMTI2ODM1ODUw._V1_.jpg",
     title: "Tom & Jerry: The Movie",
     rating: "5.2",
+    url:"https://youtu.be/kP9TfCWaQT4",
     description: "A chaotic battle ensues between Jerry Mouse, who has taken refuge in the Royal Gate Hotel, and Tom Cat, who is hired to drive him away before the day of a big wedding arrives."
   },
   {
     pic: "https://m.media-amazon.com/images/M/MV5BN2I2Yzc2OWMtMWQzYi00ZDcxLTgyOTMtNjBiNzA5Y2QxZDYxXkEyXkFqcGdeQXVyMTM0NTc2NDgw._V1_.jpg",
     title: "Venom: Let There Be Carnage",
     rating: "6.0",
+    url:"https://youtu.be/-ezfi6FQ8Ds",
     description: "Eddie Brock attempts to reignite his career by interviewing serial killer Cletus Kasady, who becomes the host of the symbiote Carnage and escapes prison after a failed execution."
   }
 ];
@@ -109,7 +120,6 @@ function App() {
 
 function AddMovie() {
   const [Add, setMovie] = useState({pic:"",title:'',rating:"",description:""});
-
   const [list, setList] = useState(INITIAL_MOVIE_buttonST);
   const styles = {
     color: "green"
@@ -163,7 +173,7 @@ function MovieDetails(){
   return(
     <div className="main-container-info">
       <div className='movie-info'>
-        <img className="profilepic" src={movie.pic} alt={movie.title} />
+        <ReactPlayer url={movie.url} controls={true}/>
         <div className='head-info'>
           <h1 className='title'>{movie.title}</h1>
           <p className='rating' style={styles}>⭐{movie.rating}</p>
